f0c2cd577aad18eb7f73448d7679ed8a1995ecb6
akshaysrivastava113 <akshay.srivastava113@gmail.com>
2024-03-02 02:11:52 +0530

    Add new UI

--- index.js
+++ index.js
@@ -4,40 +4,11 @@ const path = require("path");
 const app = express();
 
 const PORT = 3000;
-let quote;
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, 'frontend/dist', 'index.html'));
 });
-
-app.get('/testapi', (req,res) => {
-
-    // Define the data to be sent in the request params
-    // const params = {
-    //     grant_type: 'password',
-    //     client_id: client_id,
-    //     client_secret: client_secret,
-    //     username: username,
-    //     password: password
-    //   };
-
-    // Make a POST request with Axios
-    axios.post(`https://api.kanye.rest/`)
-    .then(response => {
-    // Handle successful response
-    console.log('Response:', response);
-    quote = response.data.quote;
-    res.json({"msg": quote});
-    })
-    .catch(error => {
-    // Handle error
-    console.error('Error:', error);
-    });
-});
-
-app.get('/quote', (req,res) => {
-    res.json({"msg": quote});
-})
 
 app.listen(PORT, () => {
     console.log(`Listening on ${PORT}`);
