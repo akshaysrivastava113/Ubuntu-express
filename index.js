1ad6143d6a8b3a2af1edc6808d5d0f45ab9fe139
akshaysrivastava113 <akshay.srivastava113@gmail.com>
2024-03-02 02:17:46 +0530

    Correct config

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require("path");
 const app = express();
 
 const PORT = 3000;
-app.use(express.static(path.join(__dirname, '../frontend/dist')));
+app.use(express.static(path.join(__dirname, 'frontend/dist')));
 
 app.get('*', (req,res) => {
     res.sendFile(path.join(__dirname, 'frontend/dist', 'index.html'));
