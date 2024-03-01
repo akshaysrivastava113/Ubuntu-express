cd4386d46bb1b3aab1725ba4aeb9f9131367dbe0
akshaysrivastava113 <akshay.srivastava113@gmail.com>
2024-03-01 19:32:24 +0530

    Serve static file

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const axios = require("axios");
+const path = require("path");
 const app = express();
 
 const PORT = 3000;
 let quote;
+
 app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/testapi', (req,res) => {
 
     // Define the data to be sent in the request params
     // const params = {
