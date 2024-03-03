190b9be0feea4b2b7e372349f73b3fd88d2feebe
akshaysrivastava113 <akshay.srivastava113@gmail.com>
2024-03-03 16:51:30 +0530

    Add cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const axios = require("axios");
+const cors = require("cors");
 const path = require("path");
 const app = express();
 
 const PORT = 3000;
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'frontend/dist')));
 
 app.get('*', (req,res) => {
